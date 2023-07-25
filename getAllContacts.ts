a302e0a60729010e2bfa0b343d7308655620ab05
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2023-07-25 16:02:43 +0200

    [ADD] : Getting all contacts

---
+++ getAllContacts.ts
@@ -0,0 +1,16 @@
+async function getAllContacts(apiKey : string) {
+    const response = await fetch(
+        "https://api.hubapi.com/crm/v3/objects/contacts",
+        {
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${apiKey}`
+            }
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error(`${response.statusText} ${await response.text()}`);
+    }
+    return response.json();
+}
