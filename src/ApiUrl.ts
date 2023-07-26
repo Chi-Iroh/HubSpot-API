f7cf821fcf0029700769fc43f9a4e02d417e9791
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2023-07-26 23:12:43 +0200

    [ADD] : Type for any API URL

--- src/ApiUrl.ts
+++ src/ApiUrl.ts
@@ -1,3 +1,5 @@
 export const API_URL_BASE : string = "https://api.hubapi.com";
 
 export const API_URL_CONTACTS : string = `${API_URL_BASE}/crm/v3/objects/contacts`;
+
+export type ApiTarget = typeof API_URL_BASE | typeof API_URL_CONTACTS;
